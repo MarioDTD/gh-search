8d9e7ff9a1df39794e5dff210e3f62b8d9c50aab
Mario Stefan Juhari <msjdtd@gmail.com>
2022-02-10 18:13:08 +0800

    handle user input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [userInput, setUserInput] = useState()
+
+  function handleInput(e) {
+    const inputDelay = setTimeout(() => {
+      console.log(e.target.value)
+      return () => clearTimeout(inputDelay)
+    }, 1000)
+  }
+
   return (
     <div>
       <div className="navbar">Github Repo Search</div>
       <form action="" className="search">
-        <input className="user-input" type="text" placeholder="Search Repos" />
+        <input autoFocus onChange={handleInput} name="search" className="user-input" type="text" placeholder="Search Repos" />
       </form>
     </div>
   )
